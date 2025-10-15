2144c111f5bd1356c286782ca6790f79352e8ed9
Nnamdi Echem <nnamdi.b@gmail.com>
2025-10-15 22:19:27 +0200

    feat: add Note component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Note from "./components/Note";
 
 function App() {
   return (
     <>
       <Header />
+      <Note />
       <Footer />
     </>
   );
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <h2>Note Title</h2>
+      <p>This is the content of the note.</p>
+    </div>
+  );
+}
+
+export default Note;
