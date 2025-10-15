8db2468eaa38ca68992be2a666302ce6461d4090
Nnamdi Echem <nnamdi.b@gmail.com>
2025-10-15 22:55:34 +0200

    style: update App component structure and add header/footer styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Note from "./components/Note";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
       <Note />
       <Footer />
-    </>
+    </div>
   );
 }
 
