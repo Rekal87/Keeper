796393b606ce41c5bf11ad5c2077415b5d90fce5
Nnamdi Echem <nnamdi.b@gmail.com>
2025-10-16 14:59:36 +0200

    feat: implement note saving functionality in Note component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { useState } from "react";
 
 function Note() {
+  const [note, setNote] = useState();
+
+  const handleClick = () => {
+    setNote("Note saved!");
+  };
   return (
     <div>
       <h2>Note Title</h2>
       <p>This is the content of the note.</p>
+
+      <div>
+        <p>{note}</p>
+        <button onClick={handleClick}>Save</button>
+      </div>
     </div>
   );
 }
