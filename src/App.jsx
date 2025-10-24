f7ec43a27fb5dbe50a927207499347468e9de95a
Nnamdi Echem <nnamdi.b@gmail.com>
2025-10-24 12:01:07 +0200

    added functions and mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
 import Note from "./components/Note";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div className="App container">
       <Note />
       <Footer />
     </div>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,29 +1,50 @@
 import { useState } from "react";
+import Header from "./Header";
 
 function Note() {
-  const [note, setNote] = useState();
-  const [newDate, setNewDate] = useState();
+  // saves our tasks
+  const [keep, setKeep] = useState([
+    "Eat breakfast",
+    "Buy Milk",
+    "Walk the dogs",
+  ]);
 
-  const handleClick = () => {
-    setNote("Note saved!");
+  // new tasks thats being added
+  const [newKeep, setNewKeep] = useState("");
+
+  // fuction for the textbox when adding tasks
+  const handleInputChangr = (event) => {
+    setNewKeep(event.target.value);
   };
 
-  const handleDateChange = () => {
-    const now = new Date();
-    setNewDate(now);
-  };
+  // add tasks
+  const addKeep = () => {};
+
+  // delete tasks
+  const deleteKeep = (index) => {};
+
+  const moveKeepUp = (index) => {};
+
+  const moveKeepDown = (index) => {};
+
   return (
-    <div>
-      <h2>Note Title</h2>
-      <p>This is the content of the note.</p>
-
-      <div>
-        <p>{note}</p>
-        <button onClick={handleClick}>Save</button>
-
-        <h4>Date:</h4>
-        <p>{newDate}</p>
-        <button onClick={handleDateChange}>Get Current Date</button>
+    <div className="to-do-list">
+      <Header />
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={newKeep}
+          onChange={handleInputChangr}
+        />
+        <button
+          className="btn btn-outline-secondary"
+          type="button"
+          id="button-addTask"
+          onClick={addKeep}
+        >
+          Add
+        </button>
       </div>
     </div>
   );
